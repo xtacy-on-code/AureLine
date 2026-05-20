98410cc8cf10bff6574172172133bfb585b1ddc7
Vedant <xtacyhax@gmail.com>
2026-05-20 23:28:26 +0530

    Day1: backend express, mongodb setup

---
+++ server/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config(); 
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// test route
+app.get('/', (req, res) => {
+    res.json({ message: 'AureLine API is running'});
+});
+
+// connect database
+mongoose.connect(process.env.MONGODB_URI)
+    .then(() => console.log('MONGODB connected'))
+    .catch(err => console.error('MONGODB error: ', err))
+
+// start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
